96b409f7c33ef6a8a42eb481fa0fccedeacc5b95
Arturo Espinal <arturoespinallopez@outlook.com>
2025-03-18 19:57:44 -0600

    lo que sea

--- probando/obj/Debug/net8.0/probando.AssemblyInfo.cs
+++ probando/obj/Debug/net8.0/probando.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("probando")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+239962ab3680b55977af1b5cea9ad1dc03e51d8e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48ee1b2909e29b5f6a93bb526c431b31371c1891")]
 [assembly: System.Reflection.AssemblyProductAttribute("probando")]
 [assembly: System.Reflection.AssemblyTitleAttribute("probando")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
